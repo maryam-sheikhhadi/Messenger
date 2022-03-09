f7dd9efdce13a1eb946c2eacdc69c1a20ec8d26e
maryam-sheikhhadi <93716835+maryam-sheikhhadi@users.noreply.github.com>
2022-03-09 17:27:41 +0330

    Delete SRC/messenger_phse2 directory

--- SRC/messenger_phse2/contacts/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import Contact
-from django import forms
-
-
-class ContactModelForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['first_name', 'last_name', 'email', 'other_emails', 'phone_number', 'birth_date']
