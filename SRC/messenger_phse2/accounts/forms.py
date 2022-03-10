187cec339a2e87dc7c95bf0cdd02dfa19d79c5a2
Maryam <mrymsheikhhadi@gmail.com>
2022-03-10 23:07:46 +0330

    edited version

--- SRC/messenger_phse2/accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
-
-
-class SignupForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'birth_date', 'gender', 'country', 'password1', 'password2', 'email']
